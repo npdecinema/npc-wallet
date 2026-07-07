95a2fe967fbb0bb13a3c765dcbb8b1d2312bd78c
Leo Costa <porcupineforever@gmail.com>
2026-07-07 14:51:03 -0300

    forca ipv4 no smtp

--- src/services/email.js
+++ src/services/email.js
@@ -4,6 +4,7 @@ const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 465,
   secure: true,
+  family: 4,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_APP_PASSWORD
